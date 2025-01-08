617a6bfd61d0902baf5358cac638e43373c91792
vgonzcam <59830206+vgonzcam@users.noreply.github.com>
2025-01-08 12:52:43 +0100

    Commit

--- main.tf
+++ main.tf
@@ -9,12 +9,41 @@ terraform {
       name = "vgonzcam-btp"
     }
   }
+
+
+  cloud {
+
+    organization = "vgonzcam-btp"
+
+    workspaces {
+      name = "vgonzcam-btp"
+    }
+
+  }
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "1.8.0"
+    }
+  }
 }
+
+provider "btp" {
+  globalaccount = "c2d80553trial-ga"
+}
+
+
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
     value = "A example resource that does nothing!"
   }
+}
+
+data "btp_directories" "all" {}
+
+output "btp_directories-all" {
+  value = btp_directories.all
 }
 
