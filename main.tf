cdbd044a9a0a96d83f2d96d26409d1ee56b81ef8
vgonzcam <59830206+vgonzcam@users.noreply.github.com>
2025-01-08 12:54:31 +0100

    Commit

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # The configuration for the `remote` backend.
 terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "vgonzcam-btp"
+  #   backend "remote" {
+  #     # The name of your Terraform Cloud organization.
+  #     organization = "vgonzcam-btp"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "vgonzcam-btp"
-    }
-  }
+  #     # The name of the Terraform Cloud workspace to store Terraform state files in.
+  #     workspaces {
+  #       name = "vgonzcam-btp"
+  #     }
+  #   }
 
 
   cloud {
